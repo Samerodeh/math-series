de9e88af8d07b262d076921f56e833dd67486477
Samer Odeh <samerodeh220@gmail.com>
2021-07-27 02:27:13 +0300

    edit1 branch lab 02

--- tests/test_series.py
+++ tests/test_series.py
@@ -5,49 +5,49 @@ from math_series.series import *
 def test_version():
     assert __version__ == '0.1.0'
 
-def test_fibonacci_1():
+def test1_fibonacci():
     actual = fibonacci(0)
     expected = 0
     assert actual == expected
 
-def test_fibonacci_2():
+def test2_fibonacci():
     actual = fibonacci(1)
     expected = 1
     assert actual == expected
 
-def test_fibonacci_3():
+def test3_fibonacci():
     actual = fibonacci(2)
     expected = 1
     assert actual == expected  
 
 
-def test_lucas_1():
+def test1_lucas():
     actual = lucas(0)
     expected = 2
     assert actual == expected
 
-def test_lucas_2():
+def test2_lucas():
     actual = lucas(1)
     expected = 3
     assert actual == expected
 
-def test_lucas_3():
+def test3_lucas():
     actual = lucas(2)
     expected = 5
     assert actual == expected 
 
     
-def test_sum_series_1():
+def test1_sum_series():
     actual = sum_series(0)
     expected = 0
     assert actual == expected
 
-def test_sum_series_2():
+def test2_sum_series():
     actual = sum_series(0,0)
     expected = 0
     assert actual == expected
 
-def test_sum_series_3():
+def test3_sum_series():
     actual = sum_series(0,0,1)
     expected = 0
     assert actual == expected
